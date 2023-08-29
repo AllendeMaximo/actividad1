209ac7140df0c75ae932500f3579be25a36e9677
AllendeMaximo <130809626+AllendeMaximo@users.noreply.github.com>
2023-08-29 20:54:37 -0300

    se crea el nabar con sus style

---
+++ src/components/nabvar.js
@@ -0,0 +1,15 @@
+import '../index.css';
+function Nabvar() {
+    return (
+        <nav className="navbar">
+          <div className="logo">Logo</div>
+          <ul className="nav-list">
+            <li>Inicio</li>
+            <li>Servicios</li>
+            <li>Contacto</li>
+          </ul>
+        </nav>
+      );
+    };
+
+export default Nabvar;
